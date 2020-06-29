ee69261220f94a8427f41e19ca1514c09db16967
Mike Green <m.d.j.green@ntlworld.com>
2020-06-29 09:33:31 +0000

    drinkAbout challeng testing

---
+++ spec/drinkAboutSpec.js
@@ -0,0 +1,14 @@
+describe("drinkAbout",function(){
+    describe ("whatCanIDrink tests",function(){
+        it ("should return an error if age is wrong", function(){
+            expect (whatCanIDrink(-1)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+            expect (whatCanIDrink(130)).toBe("Sorry. I can’t tell what drink because that age is incorrect!")
+        })
+        it ("should return a drink", function(){
+            expect (whatCanIDrink(10)).toBe("Drink Toddy");
+            expect (whatCanIDrink(17)).toBe("Drink Coke");
+            expect (whatCanIDrink(20)).toBe("Drink Beer");
+            expect (whatCanIDrink(40)).toBe("Drink Rum");   /* Deliberate should be whisky */
+        })
+    })
+})
